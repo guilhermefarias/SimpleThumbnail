203c5552e9c533c93de2533ba8c702d74274c9c1
Guilherme Farias <guilherme@guiky.com.br>
2013-12-27 20:47:53 -0300

    First commit

---
+++ SimpleThumbnail.php
@@ -0,0 +1,52 @@
+<?php
+
+class SimpleThumbnail {
+	private $width;
+	private $height;
+	private $quality;
+
+	function setWidth($width){
+		$this->width = $width;
+	}
+
+	function setHeight($height){
+		$this->height = $height;
+	}
+
+	function setQuality($quality){
+		$this->quality = $quality;
+	}
+
+	function generate($sourceImage, $targetImage){
+		$image = imagecreatefromstring(file_get_contents($sourceImage));
+
+		$imageWidth = imagesx($image);
+		$imageHeight = imagesy($image);
+
+		$thumbWidth = $this->width;
+		$thumbHeight = $this->height;
+		$thumbQuality = $this->quality;
+
+		$originalRatio = $imageWidth / $imageHeight;
+		$thumbRatio = $thumbWidth / $thumbHeight;
+
+		if ( $originalRatio >= $thumbRatio ){
+			$newHeight = $thumbHeight;
+			$newWidth = $imageWidth / ($imageHeight / $thumbHeight);
+		}
+		else {
+			$newWidth = $thumbWidth;
+			$newHeight = $imageHeight / ($imageWidth / $thumbWidth);
+		}
+
+		$horizontalOffset = 0 - ($newWidth - $thumbWidth) / 2;
+		$verticalOffset = 0 - ($newHeight - $thumbHeight) / 2;
+
+		$thumb = imagecreatetruecolor( $thumbWidth, $thumbHeight );
+
+		imagecopyresampled($thumb, $image, $horizontalOffset, $verticalOffset, 0, 0, $newWidth, $newHeight, $imageWidth, $imageHeight);
+
+		imagejpeg($thumb, $targetImage, $thumbQuality);
+	}
+}
+?>
